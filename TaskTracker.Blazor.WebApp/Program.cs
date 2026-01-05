91f769ce9bcf1afd0f11b2284acbad6a21491e66
Alina <alina.koval@lnu.edu.ua>
2026-01-05 10:39:23 +0200

    Move services and refit registrations to extension method

--- TaskTracker.Blazor.WebApp/Program.cs
+++ TaskTracker.Blazor.WebApp/Program.cs
@@ -3,13 +3,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
 using Blazored.LocalStorage;
-using Refit;
 using TaskTracker.Blazor.WebApp;
 using TaskTracker.Blazor.WebApp.Authentication;
-using TaskTracker.Blazor.WebApp.Handlers;
-using TaskTracker.Blazor.Services;
+using TaskTracker.Blazor.Services; 
 using TaskTracker.Blazor.Services.Abstraction;
-using TaskTracker.Blazor.Services.Abstraction.ExternalApi;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -18,43 +15,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiBaseUrl = builder.Configuration["ApiSettings:BaseUrl"]
                  ?? "https://localhost:7163/api";
 
+
 builder.Services.AddBlazoredLocalStorage();
-
 builder.Services.AddMudServices();
-
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-
 builder.Services.AddScoped<IAppStorageService, LocalStorageService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IBoardService, BoardService>();
-builder.Services.AddScoped<ITaskService, TaskService>();
-builder.Services.AddScoped<ICommentService, CommentService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ITaskListService, TaskListService>();
 
-
-builder.Services.AddTransient<AuthorizationMessageHandler>();
-
-
-builder.Services.AddRefitClient<IAuthApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiBaseUrl));
-builder.Services.AddRefitClient<IBoardApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiBaseUrl))
-    .AddHttpMessageHandler<AuthorizationMessageHandler>();
-builder.Services.AddRefitClient<ITaskApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiBaseUrl))
-    .AddHttpMessageHandler<AuthorizationMessageHandler>();
-builder.Services.AddRefitClient<ICommentApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiBaseUrl))
-    .AddHttpMessageHandler<AuthorizationMessageHandler>();
-builder.Services.AddRefitClient<IUserApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiBaseUrl))
-    .AddHttpMessageHandler<AuthorizationMessageHandler>();
-builder.Services.AddRefitClient<ITaskListApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri(apiBaseUrl))
-    .AddHttpMessageHandler<AuthorizationMessageHandler>();
-
+builder.Services.AddApplicationServices(apiBaseUrl);
 
 await builder.Build().RunAsync();
